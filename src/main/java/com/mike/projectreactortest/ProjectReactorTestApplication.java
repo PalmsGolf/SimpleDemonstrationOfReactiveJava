1c3c2045d3b8f3882c4888d3c014d4562c2d2988
Mykhailo Kaminetskyi <m.kaminetskyi@levi9.com>
2022-07-17 20:41:37 +0300

    Creates project

---
+++ src/main/java/com/mike/projectreactortest/ProjectReactorTestApplication.java
@@ -0,0 +1,13 @@
+package com.mike.projectreactortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectReactorTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectReactorTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mike/projectreactortest/ProjectReactorTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mike.projectreactortest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectReactorTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
